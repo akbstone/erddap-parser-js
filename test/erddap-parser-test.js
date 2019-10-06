db75946d7d1c85ac833719bb24041c17941a36de
unknown <brian@axiomdatascience.com - laptop>
2019-10-06 01:36:55 -0800

    get actual test working with mock metadata

--- test/erddap-parser-test.js
+++ test/erddap-parser-test.js
@@ -1,28 +1,47 @@
 const tape = require("tape"),
-	  erddap_parser = require("../").erddap_parser;
+	  erddapParser = require("../").erddapParser;
 
 
 
 
-tape("parse_tabledap_metadata() formats headers correctly", function(test){
+tape("parseTabledapMetadata() formats headers correctly", function(test){
 
 
-	var metadata_csv = [],//mock metadata csv
-		metadata = erddap_parser.parse_tabledap_metadata(metadata_csv);
+	var metadata_csv = [
+			{"Row Type": "variable", "Variable Name": "air_temperature", "Attribute Name": "", "Data Type": "double", Value: "", _FillValue: "-9999.99", id: "322100", ioos_category: "Other", long_name: "Air Temperature", missing_value: "-9999.99", platform: "station", standard_name: "air_temperature", units: "degree_Celsius", urn: "http://mmisw.org/ont/cf/parameter/air_temperature"},
+			{"Row Type": "attribute", "Variable Name": "air_temperature", "Attribute Name": "_FillValue", "Data Type": "double", Value: "-9999.99"},
+			{"Row Type": "attribute", "Variable Name": "air_temperature", "Attribute Name": "id", "Data Type": "String", Value: "322100"},
+			{"Row Type": "attribute", "Variable Name": "air_temperature", "Attribute Name": "ioos_category", "Data Type": "String", Value: "Other"},
+			{"Row Type": "attribute", "Variable Name": "air_temperature", "Attribute Name": "long_name", "Data Type": "String", Value: "Air Temperature"},
+			{"Row Type": "attribute", "Variable Name": "air_temperature", "Attribute Name": "missing_value", "Data Type": "double", Value: "-9999.99"},
+			{"Row Type": "attribute", "Variable Name": "air_temperature", "Attribute Name": "platform", "Data Type": "String", Value: "station"},
+			{"Row Type": "attribute", "Variable Name": "air_temperature", "Attribute Name": "standard_name", "Data Type": "String", Value: "air_temperature"},
+			{"Row Type": "attribute", "Variable Name": "air_temperature", "Attribute Name": "units", "Data Type": "String", Value: "degree_Celsius"},
+			{"Row Type": "attribute", "Variable Name": "air_temperature", "Attribute Name": "urn", "Data Type": "String", Value: "http://mmisw.org/ont/cf/parameter/air_temperature"}
+		],//mock metadata csv
+		metadata = erddapParser.parseTabledapMetadata(metadata_csv);
 
-	test.equal(metadata,22)
+
+	test.equal(metadata[0]['Variable Name'],'air_temperature');
+	test.equal(metadata[0]['long_name'],'Air Temperature');
+	test.equal(metadata[0]['units'],'degree_Celsius');
 	test.end();
 })
 
 
 
-tape("parse_tabledap_data() formats data correctly", function(test){
+tape("parseTabledapData() formats data correctly", function(test){
 
 
-	var metadata_csv = [],//mock metadata csv
-		data_csv = [],
-		metadata = erddap_parser.parse_tabledap_metadata(metadata_csv),
-		data = erddap_parser.parse_tabledap_data(data_csv,data)
+	var metadata_csv = [
+
+		],//mock metadata csv
+		data_csv = [
+
+
+		],
+		metadata = erddapParser.parseTabledapMetadata(metadata_csv),
+		data = erddapParser.parseTabledapData(data_csv,data)
 
 	test.equal(data,33)
 	test.end();
