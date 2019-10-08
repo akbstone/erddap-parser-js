f96ba89e275ec4034eb3a42f62660759a0b59cbd
unknown <brian@axiomdatascience.com - laptop>
2019-10-08 10:20:55 -0400

    include dependencies in build

--- rollup.dist.config.js
+++ rollup.dist.config.js
@@ -1,6 +1,11 @@
+import resolve from 'rollup-plugin-node-resolve';
+
 export default {
   input: 'src/index.js',
-  external: ['d3-fetch'],
+  //external: ['d3-fetch'],
+  plugins:[
+    resolve()
+  ],
   output:{
     format: 'umd',
     name:'d3',
